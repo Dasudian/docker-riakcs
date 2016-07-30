89f2d5296f78c1df5d78d71868cbd7e391d3c5b5
Mengz <mz@dasudian.com>
2016-07-30 11:26:55 +0800

    add some hint output when start the container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ chown riak:riak /var/lib/riak
 
 if [[ "$1" = "supervisord" ]]; then
   echo "Run the container in supervisord mode."
+  echo ""
+  echo "Note: After the init-riakcs finished, pleasce use below command to check the init result:"
+  echo "`docker exec -it [container_name or id] cat /init-riakcs.log`"
+  echo ""
   supervisord -c /etc/supervisor/supervisord.conf
 else
   exec $@
